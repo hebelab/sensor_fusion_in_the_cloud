b6bb5434d9aae4143adced7f0d7b09ffbf6fa8fa
ayusufsirin <65160352+ayusufsirin@users.noreply.github.com>
2023-03-09 19:55:11 +0300

    Python files are organized

--- Python/utils/PG.py
+++ Python/utils/PG.py
@@ -35,7 +35,7 @@ def pg(input, us_rate, ncutoff, gt_mean, threshold=100):
     filtered = input
 
     while pg_mean * 1.1 < gt_mean and threshold > 0:
-        filtered = lpf(filtered, ncutoff, us_rate)
+        filtered = lpf(filtered, ncutoff)
         filtered[::us_rate, ::us_rate] = input[::us_rate, ::us_rate]
         pg_mean = filtered.mean()
 
