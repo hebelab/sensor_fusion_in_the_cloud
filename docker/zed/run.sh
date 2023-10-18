d64ffff2af998d03ce2351a1fc2a07e28bf02795
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2023-10-18 07:41:07 +0300

    Updated Docker ZED

--- docker/zed/run.sh
+++ docker/zed/run.sh
@@ -1,10 +1,11 @@
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
+sudo apt update -y
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
 sudo rosdep init
-rosdep update
+sudo rosdep update
 
 # Install Cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
