4c707879502da915e8d42590d42acbc6188da30c
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2023-10-18 07:45:54 +0300

    Update Docker RTK

--- docker/rtk/entrypoint.sh
+++ docker/rtk/entrypoint.sh
@@ -2,6 +2,11 @@
 
 source /opt/ros/noetic/setup.bash
 cd ~/catkin_ws
+
+# rm -rf build/ devel/
+# catkin_make -DPYTHON_VERSION=3
+# pip install -r src/rtk-ros-wrapper/requirements.txt
+
 source devel/setup.bash
 
 roslaunch rtk_wrapper RTK.launch
